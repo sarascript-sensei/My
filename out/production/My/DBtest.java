a54e75e1a39a5f9fa714c34c61c39322d44cd78f
sarascript-sensei <sara.2000year14@gmail.com>
2021-03-25 01:49:27 +0600

    Something

--- out/production/My/DBtest.java
+++ out/production/My/DBtest.java
@@ -1,5 +1,6 @@
-package src;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -9,17 +10,16 @@ public class DBtest {
         String name_db = "Sara";
         String password = "12345s";
         Scanner in = new Scanner(System.in);
-
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Логин: ");
         String loginInsert = in.next();
 
         System.out.println("Пароль : ");
         String passwordInsert = in.next();
 
-        try(Connection conn = DriverManager.getConnection(url,name_db,password);
-            Statement myState = conn.createStatement()
-        )
-        {
+        try (Connection conn = DriverManager.getConnection(url, name_db, password);
+             Statement myState = conn.createStatement()
+        ) {
             PreparedStatement statement = conn.prepareStatement("INSERT INTO login2 (login_name, pass) VALUES (?, ?)");
 
             statement.setString(1, loginInsert);
@@ -28,7 +28,17 @@ public class DBtest {
             System.out.println(rows);
             System.out.println("Пользователь создан!");
 
-        }
+            System.out.println("\nЖелаете войти в систему?: ");
+            do {
+                String choice = bf.readLine();
+                if (choice.startsWith("n")) {
+                    break;
+                }
+            }
+            while (true);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
